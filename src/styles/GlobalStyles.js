026a6ce823bcf28ccf8e7a27bef6b11de52a970a
xDouglas90 <xdouglas90@gmail.com>
2022-08-11 20:52:06 -0300

    :wrench: chore: add react-icons #1

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
-export const GlobalStyle = createGlobalStyle`
+export const GlobalStyles = createGlobalStyle`
     :root {
         font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
         font-size: 16px;
@@ -14,20 +14,27 @@ export const GlobalStyle = createGlobalStyle`
         -webkit-text-size-adjust: 100%;
     }
 
-    * {
+    *, *::after, *::before {
         box-sizing: border-box;
     }   
+
+    html {
+        @media (max-width: 1080px) {
+            font-size: 93.75%; // 15px
+        }
+        @media (max-width: 720px) {
+            font-size: 87.5%; // 14px
+        }
+    }
 
     body {
         margin: 0;
         padding: 0;
-        font-family: inherit;
-        font-size: inherit;
-        line-height: inherit;
-        font-weight: inherit;
-        color: inherit;
-        background-color: inherit;
     }
+
+    h1, h2, h3, h4, h5, h6, strong {
+        font-weight: 700;
+    } 
 
     a {
         color: inherit;
@@ -36,15 +43,14 @@ export const GlobalStyle = createGlobalStyle`
 
     button, input, select, textarea {
         appearance: none;
-        font-family: inherit;
-        font-size: inherit;
-        line-height: inherit;
-        font-weight: inherit;
-        color: inherit;
-        background-color: inherit;
         border: none;
         outline: none;
         cursor: pointer;
         padding: 0;
+    }
+
+    [disabled] {
+        opacity: 0.6;
+        cursor: not-allowed;
     }
 `;
