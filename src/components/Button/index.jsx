7a6b90679c3f57300b13c3c62bfeb0907c674548
xDouglas90 <xdouglas90@gmail.com>
2022-08-11 22:16:01 -0300

    :sparkles: feat: add prop types in the Button component #3

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,5 +1,11 @@
-import { Btn } from "./styles";
+import PropTypes from 'prop-types';
+import { Btn } from './styles';
 
 export const Button = ({ type, children }) => {
   return <Btn type={type}> {children} </Btn>;
+};
+
+Button.propTypes = {
+  type: PropTypes.oneOf(['primary', 'secondary']).isRequired,
+  children: PropTypes.node.isRequired,
 };
